4b52846505105b65fafe4bb01b8e82c593b8a571
TobiasReich <mad.scientist@freenet.de>
2021-12-18 18:38:07 +0100

    WIP Adding a first draft for a game configuration (mocked only for now)

--- src/main/kotlin/de/tobiasreich/kaiser/UIControllerStartScreen.kt
+++ src/main/kotlin/de/tobiasreich/kaiser/UIControllerStartScreen.kt
@@ -2,22 +2,35 @@ package de.tobiasreich.kaiser
 
 import de.tobiasreich.kaiser.game.Game
 import de.tobiasreich.kaiser.game.Player
+import de.tobiasreich.kaiser.game.PlayerConfig
 import de.tobiasreich.kaiser.game.data.player.CountryName
 import javafx.event.ActionEvent
 import javafx.fxml.FXML
+import javafx.fxml.Initializable
+import javafx.scene.control.TableColumn
+import javafx.scene.control.TableView
+import javafx.scene.control.cell.PropertyValueFactory
 import javafx.scene.layout.BorderPane
+import javafx.scene.paint.Color
+import java.net.URL
+import java.util.*
 
 
-class UIControllerStartScreen {
+class UIControllerStartScreen : Initializable {
 
     @FXML
     private lateinit var rootBorderPane: BorderPane
+
+    @FXML
+    private lateinit var configTable: TableView<PlayerConfig>
 
     /********************************************
      *
      *             Game Menu items
      *
      *******************************************/
+
+
 
     @FXML
     fun onStartGameClick(actionEvent: ActionEvent) {
@@ -29,4 +42,32 @@ class UIControllerStartScreen {
         ViewController.showScene(ViewController.SCENE_NAME.GAME)
     }
 
+    override fun initialize(p0: URL?, p1: ResourceBundle?) {
+//        val player0 = PlayerConfig(0, CountryName.HOLSTEIN , "Alice", Color.RED,true, true, 0)
+//        val player1 = PlayerConfig(2, CountryName.BAVARIA , "Bob", Color.RED,false, true, 0)
+//
+//        val indexColumn = TableColumn<PlayerConfig, Int>("Index!!!")
+//        indexColumn.cellValueFactory = PropertyValueFactory("id")
+//
+//        val nameColumn = TableColumn<PlayerConfig, String>("Spieler Name!!!")
+//        nameColumn.cellValueFactory = PropertyValueFactory("name")
+//
+//        configTable.columns.add(indexColumn)
+//        configTable.columns.add(nameColumn)
+//
+//        configTable.items.add(player0)
+//        configTable.items.add(player1)
+    }
+
+    /*
+    *  <columns>
+            <TableColumn prefWidth="27.0" text="#" />
+          <TableColumn prefWidth="71.0" text="Land" />
+          <TableColumn prefWidth="127.0" text="Name" />
+            <TableColumn prefWidth="75.0" text="Farbe" />
+            <TableColumn prefWidth="75.0" text="Männlich" />
+            <TableColumn prefWidth="75.0" text="Aktiv" />
+            <TableColumn prefWidth="102.0" text="Schwierigkeit" />
+        </columns>
+    * */
 }
--- src/main/kotlin/de/tobiasreich/kaiser/game/data/player/CountryName.kt
+++ src/main/kotlin/de/tobiasreich/kaiser/game/data/player/CountryName.kt
@@ -8,12 +8,11 @@ package de.tobiasreich.kaiser.game.data.player
  *                  Baden      Franken
  *                      Bayern
  */
-enum class CountryName {
+enum class CountryName(val nameResource: String) {
 
-    HOLSTEIN,
-    WESTPHALIA, SAXONY, PRUSSIA,
-    HESSE, THURINGIA, BOHEMIA,
-    BADEN, FRANCONIA,
-    BAVARIA
-
+    HOLSTEIN("%game_country_holstein"),
+    WESTPHALIA("%game_country_westphalia"), SAXONY("%game_country_saxony"), PRUSSIA("%game_country_prussia"),
+    HESSE("%game_country_hesse"), THURINGIA("%game_country_thuringia"), BOHEMIA("%game_country_bohemia"),
+    BADEN("%game_country_baden"), FRANCONIA("%game_country_franconia"),
+    BAVARIA("%game_country_bavaria")
 }
