2921a8dccaed4babc94e6a1546e1cad44f59dd35
TobiasReich <mad.scientist@freenet.de>
2021-12-19 23:06:38 +0100

    First attempt on how to implement a help documentation

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Kaiser3
-A remake of the remake of the original 1985 Kaiser game
+
+A remake of the remake of the original C64/Amiga classic 1984 Kaiser game (developed by Markus Mergard, distributed by Ariolasoft) as well as the inofficial remake "Kaiser II" by Stefan Maday (1997-2004)
+
+This is no commercial project but more a sign of respect for the original game which provided endless hours of fun when I was a kid. If you enjoyed the game as much as I did, feel free to help with that remake!
--- src/main/kotlin/de/tobiasreich/kaiser/UIControllerGameMenu.kt
+++ src/main/kotlin/de/tobiasreich/kaiser/UIControllerGameMenu.kt
@@ -1,24 +1,19 @@
 package de.tobiasreich.kaiser
 
 import de.tobiasreich.kaiser.game.Game
-import javafx.event.ActionEvent
-import javafx.event.EventHandler
 import javafx.fxml.FXML
 import javafx.fxml.FXMLLoader
 import javafx.fxml.Initializable
 import javafx.scene.Scene
-import javafx.scene.chart.BarChart
-import javafx.scene.chart.XYChart
-import javafx.scene.chart.XYChart.Series
-import javafx.scene.control.Label
-import javafx.scene.image.ImageView
-import javafx.scene.layout.BorderPane
+import javafx.scene.web.WebView
 import javafx.stage.Modality
 import javafx.stage.Stage
 import java.net.URL
 import java.util.*
 
-
+/** This is the controller for the game menu
+ *
+ */
 class UIControllerGameMenu : Initializable {
 
 
@@ -62,11 +57,32 @@ class UIControllerGameMenu : Initializable {
 
     @FXML
     private fun onMenuHelpClick() {
+        val fxmlLoader = FXMLLoader(Main::class.java.getResource("menu-help-webview.fxml"), Game.stringsBundle)
+        val helpScene = Scene(fxmlLoader.load())
+
+        val stage = Stage()
+        stage.initModality(Modality.APPLICATION_MODAL)
+        stage.title = "Hilfe"
+        stage.scene = helpScene
+        stage.show()
+
+        val helpWindowWebview = helpScene.lookup("#helpWindowWebview") as WebView
+        //TODO This should also be affected by I18n so we have to create translated help html pages
+        val url = javaClass.getResource("help_index.html")
+        helpWindowWebview.engine.load(url.toExternalForm())
     }
 
 
     @FXML
     private fun onMenuAboutClick() {
+        val fxmlLoader = FXMLLoader(Main::class.java.getResource("menu-help-about.fxml"), Game.stringsBundle)
+        val helpScene = Scene(fxmlLoader.load(), 640.0, 480.0)
+
+        val stage = Stage()
+        stage.initModality(Modality.APPLICATION_MODAL)
+        stage.title = Game.stringsBundle.getString("menu_about_title")
+        stage.scene = helpScene
+        stage.show()
     }
 
 
